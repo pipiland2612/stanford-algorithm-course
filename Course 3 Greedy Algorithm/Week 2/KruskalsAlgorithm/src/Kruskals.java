f6a68cf347d1edb071d9de7ee8658dc9a702f5fe
pipiland2612 <nguyen.t.dang.minh@gmail.com>
2024-12-12 14:20:10 +0200

    Fix

--- Course 3 Greedy Algorithm/Week 2/KruskalsAlgorithm/src/Kruskals.java
+++ Course 3 Greedy Algorithm/Week 2/KruskalsAlgorithm/src/Kruskals.java
@@ -32,10 +32,6 @@ class Graph{
             rank[i] = 0;
         }
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> 5104537 (Fix)
     public void addEdge(int start, int dest, int weight){
         Edge e = new Edge(start, dest, weight);
         edges.add(e);
@@ -44,13 +40,8 @@ class Graph{
     public int find(int vertex){
         if(vertex < 0 || vertex >= vertices)
             throw new IllegalArgumentException("Vertex out of bounds: " + vertex);
-<<<<<<< HEAD
         while (vertex != parents[vertex]){
             // Path compression
-=======
-
-        while (vertex != parents[vertex]){
->>>>>>> 5104537 (Fix)
             parents[vertex] = parents[parents[vertex]];
             vertex = parents[vertex];
         }
@@ -62,17 +53,10 @@ class Graph{
         int rootQ = find(q);
 
         if(rootP == rootQ)return;
-<<<<<<< HEAD
-
         //Optimization: merge smaller tree to the larger one
         if(rank[rootP] < rank[rootQ]){
             parents[rootP] = rootQ;
         }else{
-=======
-        if(rank[rootP] < rank[rootQ]){
-            parents[rootP] = rootQ;
-        }else {
->>>>>>> 5104537 (Fix)
             parents[rootQ] = rootP;
             if(rank[rootP] == rank[rootQ])
                 rank[rootP]++;
@@ -80,17 +64,10 @@ class Graph{
     }
 
     public void kruskalMST(){
-<<<<<<< HEAD
+
         edges.sort(Comparator.comparingInt(a -> a.weight));
-        List<Edge> mst = new ArrayList<>();
-
-        int index = 0;
-=======
-        edges.sort(Comparator.comparing(e -> e.weight));
         int index = 0;
         List<Edge> mst = new ArrayList<>();
-
->>>>>>> 5104537 (Fix)
         while (mst.size() < vertices - 1){
             Edge edge = edges.get(index++);
             int src = find(edge.start);
@@ -107,12 +84,11 @@ class Graph{
 
 public class Kruskals {
     public static void main(String[] args) {
-<<<<<<< HEAD
         Graph g = new Graph(6);
         g.addEdge(0, 1, 4); g.addEdge(0, 2, 3); g.addEdge(1, 2, 1); g.addEdge(1, 3, 2);
         g.addEdge(2, 4, 4); g.addEdge(3, 4, 5); g.addEdge(4, 5, 1);
         g.kruskalMST();
-=======
->>>>>>> 5104537 (Fix)
     }
+
 }
+
